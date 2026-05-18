907d38bf59f94e9f36c4b06ed63fb74b5d4e0cbb
jorwash46-design <jorwash46@gmail.com>
2026-05-17 20:56:39 -0500

    migrar a Eleventy con Netlify build

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+
+  // Copiar assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Colección de ensayos
+  eleventyConfig.addCollection("ensayos", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/ensayos/*.md").reverse();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts"
+    },
+    templateFormats: ["md", "html", "liquid"],
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "liquid"
+  };
+};
